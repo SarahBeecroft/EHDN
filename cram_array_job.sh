92f46637efc8c654839d0cc5ecc163eb420f83ac
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2020-05-19 14:55:23 +0800

    Create cram_array_job.sh
    slurm array script that is launched by cram_str_helper.sh

---
+++ cram_array_job.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --nodes=2
+#SBATCH --cpus-per-task=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=cram-str
+#SBATCH --partition=workq 
+#SBATCH --account=pawsey0001
+#SBATCH --output=cram-str-%j.log
+#SBATCH --error=cram-str-%J.log
+
+echo "All jobs in this array have:"
+echo "- SLURM_ARRAY_JOB_ID=${SLURM_ARRAY_JOB_ID}"
+echo "- SLURM_ARRAY_TASK_COUNT=${SLURM_ARRAY_TASK_COUNT}"
+echo "- SLURM_ARRAY_TASK_MIN=${SLURM_ARRAY_TASK_MIN}"
+echo "- SLURM_ARRAY_TASK_MAX=${SLURM_ARRAY_TASK_MAX}"
+ 
+echo "This job in the array has:"
+echo "- SLURM_JOB_ID=${SLURM_JOB_ID}"
+echo "- SLURM_ARRAY_TASK_ID=${SLURM_ARRAY_TASK_ID}"
+
+# grab our filename from a directory listing
+FILES=($(ls -1 *.cram))
+FILENAME=${FILES[$SLURM_ARRAY_TASK_ID]}
+echo "My input file is ${FILENAME}"
+
+module load singularity
+
+basedir=/scratch/pawsey0339/sbeecroft/broad
+container=/group/pawsey0339/sbeecroft/expansion-hunter-denovo_v0.8.7.sif
+ref=Homo_sapiens_assembly38.fasta
+singularity exec ${container} /ExpansionHunterDenovo profile \
+        --reads ${basedir}/${FILENAME} \
+        --reference ${ref} \
+        --output-prefix ${basedir}/str-profiles/${FILENAME} \
+        --min-anchor-mapq 50 \
+        --max-irr-mapq 40
