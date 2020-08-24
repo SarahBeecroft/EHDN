d8492d7c6f47a90811466e8a30a8b3d0a80a26e4
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2020-08-24 15:59:40 +0800

    Update single_outlier_locus.sh

--- single_outlier_locus.sh
+++ single_outlier_locus.sh
@@ -9,8 +9,9 @@
 #SBATCH --error=cram-outlier-locus-%J.log
 
 module load singularity
+container=/group/pawsey0339/sbeecroft/expansionhunterdenovo_v1.sif
 
-singularity exec expansionhunterdenovo_latest.sif /ExpansionHunterDenovo/scripts/outlier.py locus \
+singularity exec $container /ExpansionHunterDenovo-v0.8.7-linux_x86_64/scripts/outlier.py locus \
         --manifest manifest.tsv \
         --multisample-profile broad_dataset1.multisample_profile.json \
         --output broad_dataset1.outlier_locus.tsv
